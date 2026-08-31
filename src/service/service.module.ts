db70b378fb777336ad1a52e54432285a99bf3876
acorn497 <acorn497@naver.com>
2026-08-31 23:14:20 +0900

    Fix: TunnelModule의 프로바이더 재선언으로 게이트웨이 인스턴스가 갈라지던 문제 수정

--- src/service/service.module.ts
+++ src/service/service.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServiceLifecycleService } from './service-lifecycle.service';
 import { ServiceGateway } from './service.gateway';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/share/prisma.module';
 import { ServiceController } from './v1/service.controller';
 import { DockerModule } from 'src/docker/docker.module';
 
 @Module({
-  imports: [ConfigModule, DockerModule, PrismaModule],
+  imports: [DockerModule, PrismaModule],
   providers: [ServiceLifecycleService, ServiceGateway],
   controllers: [ServiceController],
   exports: [ServiceLifecycleService, ServiceGateway],
 })
-
 export class ServiceModule { }
--- src/tunnel/tunnel.module.ts
+++ src/tunnel/tunnel.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { CoreModule } from 'src/core.module';
+import { DockerModule } from 'src/docker/docker.module';
+import { NotifyModule } from 'src/notify/notify.module';
+import { ServiceModule } from 'src/service/service.module';
+import { PrismaModule } from 'src/share/prisma.module';
+import { TerminalModule } from 'src/terminal/terminal.module';
+import { UtilityModule } from 'src/utility/utility.module';
 import { ReverseTunnelService } from './reverse-tunnel.service';
-import { ConfigModule } from '@nestjs/config';
+import { TunnelService } from './tunnel.service';
 
 @Module({
-  imports: [ConfigModule],
-  providers: [ReverseTunnelService],
-  exports: [ReverseTunnelService],
+  imports: [
+    CoreModule,
+    DockerModule,
+    NotifyModule,
+    ServiceModule,
+    PrismaModule,
+    TerminalModule,
+    UtilityModule,
+  ],
+  providers: [TunnelService, ReverseTunnelService],
+  exports: [TunnelService, ReverseTunnelService],
 })
 export class TunnelModule {}
